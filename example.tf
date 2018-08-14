200fceece35d2f3f9ab2647f7e073f6701641953
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2018-08-14 23:06:05 +0900

    Add provisioner, a local one

--- example.tf
+++ example.tf
@@ -4,6 +4,9 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
